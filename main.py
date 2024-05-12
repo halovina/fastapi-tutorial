c296780fb591ca2de5394880a8f52573b46f70fa
alfin <alfin@linkaja.id>
2024-05-12 12:00:08 +0700

    fastapi mysql connection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from db_config import get_db
+from models import Users
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -8,3 +11,9 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/users")
+def get_all_users(db: Session=Depends(get_db)):
+    users = db.query(Users).all()
+    return users
---
+++ models.py
@@ -0,0 +1,9 @@
+from db_config import Base
+from sqlalchemy import Column, Integer, String
+
+class Users(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
